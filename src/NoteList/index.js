d56aa130dc5b1150c95c9d3826ada4d8deb42468
Angelos Ioannou <angelos.ioannou@gmx.de>
2020-05-10 19:01:58 +0200

    note list animation

--- src/NoteList/index.js
+++ src/NoteList/index.js
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getTodos, addTodo } from "./state";
+import { connect } from "react-redux";
+import "./style.css";
+import posed from "react-pose";
+import { DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
 
-export default function NoteList() {
-  return <div>The note list will be displayed here</div>;
+const TodoList = posed.div({
+    start: {},
+    end: { staggerChildren: 150 },
+});
+
+const TodoLogo = posed.div({
+    start: { rotate: 0, opacity: 0 },
+    end: { rotate: 360, opacity: 1 },
+});
+
+const TodoVeil = posed.div({
+    start: { width: "100%" },
+    end: { width: "0%", display: "none" },
+});
+
+function NoteList({ todos, addTodo }) {
+    const [todoAnim, setTodoAnim] = useState("start");
+
+    useEffect(() => {
+        setTodoAnim("end");
+    }, []);
+
+    return (
+        <TodoList pose={todoAnim} className="todo-list">
+            {todos.map((todo, i) => (
+                <div className="todo-item" key={todo}>
+                    <TodoLogo className="todo-logo flex-center">{i+1}</TodoLogo>
+                    <div className="todo-text">
+                        {todo}
+                        <TodoVeil className="todo-veil" />
+                    </div>
+                </div>
+            ))}
+        </TodoList>
+    );
 }
+
+const mapState = (state) => ({
+    todos: getTodos(state),
+});
+
+const mapDispatch = {
+    addTodo,
+};
+
+export default connect(mapState, mapDispatch)(NoteList);
--- src/NoteList/state.js
+++ src/NoteList/state.js
@@ -1,36 +1,36 @@
 const name = "notelist";
 
 // Constants
-const ACTION = `${name}/ACTION`;
+const ADD_TODO = `${name}/ADD_TODO`;
 
 // Reducer
 const initialState = {
-  someKey: "some Value"
+    todos: ["display todos", "style them", "animate them", "implement 'add todo' functionality"],
 };
 
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ACTION:
-      return {
-        ...state,
-        ...action.payload
-      };
+    switch (action.type) {
+        case ADD_TODO:
+            return {
+                ...state,
+                todos: [...state.todos, action.payload],
+            };
 
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 };
 
 // Action Creator
-export const commitAction = payload => ({
-  type: ACTION,
-  payload
+export const addTodo = (payload) => ({
+    type: ADD_TODO,
+    payload,
 });
 
 // Selector
-export const getSomeKey = state => state[name].someKey || initialState.someKey;
+export const getTodos = (state) => state[name].todos || initialState.todos;
 
 export default {
-  reducer,
-  name
+    reducer,
+    name,
 };
