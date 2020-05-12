00e9aed678b6aaa636f4d6244cce00288bfac8b9
Angelos Ioannou <angelos.ioannou@gmx.de>
2020-05-12 18:05:19 +0200

    changes links

--- src/App/MainMenu.js
+++ src/App/MainMenu.js
@@ -3,52 +3,52 @@ import posed from "react-pose";
 import { Link } from "react-router-dom";
 
 const ItemContainer = posed.div({
-  start: {},
-  end: {
-    staggerChildren: 75
-  }
+    start: {},
+    end: {
+        staggerChildren: 75,
+    },
 });
 
 const RouterItem = posed(Link)({
-  start: {
-    y: -10,
-    opacity: 0
-  },
-  end: {
-    y: 0,
-    opacity: 1
-  }
+    start: {
+        y: -10,
+        opacity: 0,
+    },
+    end: {
+        y: 0,
+        opacity: 1,
+    },
 });
 
 const Item = posed.a({
-  start: {
-    y: -10,
-    opacity: 0
-  },
-  end: {
-    y: 0,
-    opacity: 1
-  }
+    start: {
+        y: -10,
+        opacity: 0,
+    },
+    end: {
+        y: 0,
+        opacity: 1,
+    },
 });
 
 export default function MainMenu() {
-  const [menuState, setMenuState] = useState("start");
+    const [menuState, setMenuState] = useState("start");
 
-  useEffect(() => {
-    setMenuState("end");
-  }, []);
+    useEffect(() => {
+        setMenuState("end");
+    }, []);
 
-  return (
-    <ItemContainer pose={[menuState]} className="main-menu">
-      <RouterItem className="menu-item" to="/notes">
-        Start
-      </RouterItem>
-      <Item className="menu-item" href="/notes">
-        Clear Cache
-      </Item>
-      <Item className="menu-item" href="http://github.com">
-        GitHub
-      </Item>
-    </ItemContainer>
-  );
+    return (
+        <ItemContainer pose={[menuState]} className="main-menu">
+            <RouterItem className="menu-item" to="/notes">
+                Start
+            </RouterItem>
+            <Item className="menu-item" href="#todo">
+                Clear Cache
+            </Item>
+            <Item className="menu-item" href="https://github.com/tigigti/beautiful-notes">
+                GitHub
+            </Item>
+        </ItemContainer>
+    );
 }
