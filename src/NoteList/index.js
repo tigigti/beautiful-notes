0148d1c460623d338f856ebc0c975b0f7de75c8d
Angelos Ioannou <angelos.ioannou@gmx.de>
2020-05-13 22:08:44 +0200

    change todo text and styles

--- src/NoteList/index.js
+++ src/NoteList/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getTodos, addTodo } from "./state";
+import { getTodos, addTodo, updateTodo } from "./state";
 import { connect } from "react-redux";
 import "./style.css";
 import posed from "react-pose";
@@ -7,70 +7,89 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import TextareaAutosize from "react-textarea-autosize";
 
 const TodoList = posed.div({
-  start: {},
-  end: { staggerChildren: 150 }
+    start: {},
+    end: { staggerChildren: 150 },
 });
 
 const TodoLogo = posed.div({
-  start: { rotate: 0, opacity: 0 },
-  end: { rotate: 360, opacity: 1 }
+    start: { rotate: 0, opacity: 0 },
+    end: { rotate: 360, opacity: 1 },
 });
 
 const TodoVeil = posed.div({
-  start: { width: "100%" },
-  end: { width: "0%", display: "none" }
+    start: { width: "100%" },
+    end: { width: "0%", display: "none" },
 });
 
-function NoteList({ todos, addTodo }) {
-  const [todoAnim, setTodoAnim] = useState("start");
+function NoteList({ todos, addTodo, updateTodo }) {
+    const [todoAnim, setTodoAnim] = useState("start");
 
-  useEffect(() => {
-    setTodoAnim("end");
-  }, []);
+    useEffect(() => {
+        setTodoAnim("end");
+    }, []);
 
-  const dragEnd = result => {
-    console.log("Drag ended", result);
-  };
+    const dragEnd = (result) => {
+        console.log("Drag ended", result);
+    };
 
-  return (
-    <DragDropContext onDragEnd={dragEnd}>
-      <Droppable droppableId={"droppable"}>
-        {provided => (
-          <TodoList pose={todoAnim} className="todo-list" {...provided.droppableProps} ref={provided.innerRef}>
-            {todos.map((todo, i) => (
-              <Draggable draggableId={todo.text} index={i} key={todo.text}>
-                {provided => (
-                  <div
-                    className="todo-item"
-                    {...provided.dragHandleProps}
-                    {...provided.draggableProps}
-                    ref={provided.innerRef}
-                  >
-                    <TodoLogo className="todo-logo flex-center">{i + 1}</TodoLogo>
-                    <div className="todo-text">
-                      {/* <input type="text" value={todo} /> */}
-                      <TextareaAutosize className="todo-input" value={todo.text} />
-                      {/* <div>{todo}</div> */}
-                      <TodoVeil className="todo-veil" />
-                    </div>
-                  </div>
+    const todoChanged = (e, index) => {
+        updateTodo({
+            text: e.target.value,
+            index,
+        });
+    };
+
+    return (
+        <DragDropContext onDragEnd={dragEnd}>
+            <Droppable droppableId={"droppable"}>
+                {(provided) => (
+                    <TodoList
+                        pose={todoAnim}
+                        className="todo-list"
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                    >
+                        {todos.map((todo, i) => (
+                            <Draggable draggableId={todo.text} index={i} key={`todo-${i}`}>
+                                {(provided) => (
+                                    <div
+                                        className="todo-item"
+                                        {...provided.dragHandleProps}
+                                        {...provided.draggableProps}
+                                        ref={provided.innerRef}
+                                        tabIndex={-1}
+                                    >
+                                        <TodoLogo className="todo-logo flex-center" tabIndex={-1}>
+                                            {i + 1}
+                                        </TodoLogo>
+                                        <div className="todo-text">
+                                            <TextareaAutosize
+                                                className="todo-input"
+                                                value={todo.text}
+                                                spellCheck={false}
+                                                onChange={(e) => todoChanged(e, i)}
+                                            />
+                                            <TodoVeil className="todo-veil" />
+                                        </div>
+                                    </div>
+                                )}
+                            </Draggable>
+                        ))}
+                        {provided.placeholder}
+                    </TodoList>
                 )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </TodoList>
-        )}
-      </Droppable>
-    </DragDropContext>
-  );
+            </Droppable>
+        </DragDropContext>
+    );
 }
 
-const mapState = state => ({
-  todos: getTodos(state)
+const mapState = (state) => ({
+    todos: getTodos(state),
 });
 
 const mapDispatch = {
-  addTodo
+    addTodo,
+    updateTodo,
 };
 
 export default connect(mapState, mapDispatch)(NoteList);
--- src/NoteList/state.js
+++ src/NoteList/state.js
@@ -2,52 +2,71 @@ const name = "notelist";
 
 // Constants
 const ADD_TODO = `${name}/ADD_TODO`;
+const UPDATE_TODO = `${name}/UPDATE_TODO`;
 
 // Reducer
 const initialState = {
-  todos: [
-    {
-      text: "display todos",
-      index: 1
-    },
-    {
-      text: "style them",
-      index: 2
-    },
-    {
-      text: "animate them",
-      index: 3
-    },
-    {
-      text: "implement 'add todo' functionality",
-      index: 4
-    }
-  ]
+    todos: [
+        {
+            text: "display todos",
+            index: 1,
+        },
+        {
+            text: "style them",
+            index: 2,
+        },
+        {
+            text: "animate them",
+            index: 3,
+        },
+        {
+            text: "implement 'add todo' functionality",
+            index: 4,
+        },
+    ],
 };
 
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return {
-        ...state,
-        todos: [...state.todos, action.payload]
-      };
+    switch (action.type) {
+        case ADD_TODO:
+            return {
+                ...state,
+                todos: [...state.todos, action.payload],
+            };
 
-    default:
-      return state;
-  }
+        case UPDATE_TODO:
+            const updatedTodos = state.todos.map((todo, i) => {
+                if (i !== action.payload.index) return todo;
+                return {
+                    ...todo,
+                    text: action.payload.text,
+                };
+            });
+            return {
+                ...state,
+                todos: [...updatedTodos],
+            };
+
+        default:
+            return state;
+    }
 };
 
 // Action Creator
-export const addTodo = payload => ({
-  type: ADD_TODO,
-  payload
+export const addTodo = (payload) => ({
+    type: ADD_TODO,
+    payload,
+});
+
+export const updateTodo = (payload) => ({
+    type: UPDATE_TODO,
+    payload,
 });
 
 // Selector
-export const getTodos = state => state[name].todos || initialState.todos;
+export const getTodos = (state) => state[name].todos || initialState.todos;
 
 export default {
-  reducer,
-  name
+    reducer,
+    name,
 };
