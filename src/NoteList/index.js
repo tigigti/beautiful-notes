12f6e76f89637c8a4a8489ab20862c22e5f797a2
Angelos Ioannou <angelos.ioannou@gmx.de>
2020-05-13 16:06:22 +0200

    textarea input and refactoring of todo state

--- src/NoteList/index.js
+++ src/NoteList/index.js
@@ -4,71 +4,73 @@ import { connect } from "react-redux";
 import "./style.css";
 import posed from "react-pose";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import TextareaAutosize from "react-textarea-autosize";
 
 const TodoList = posed.div({
-    start: {},
-    end: { staggerChildren: 150 },
+  start: {},
+  end: { staggerChildren: 150 }
 });
 
 const TodoLogo = posed.div({
-    start: { rotate: 0, opacity: 0 },
-    end: { rotate: 360, opacity: 1 },
+  start: { rotate: 0, opacity: 0 },
+  end: { rotate: 360, opacity: 1 }
 });
 
 const TodoVeil = posed.div({
-    start: { width: "100%" },
-    end: { width: "0%", display: "none" },
+  start: { width: "100%" },
+  end: { width: "0%", display: "none" }
 });
 
 function NoteList({ todos, addTodo }) {
-    const [todoAnim, setTodoAnim] = useState("start");
+  const [todoAnim, setTodoAnim] = useState("start");
 
-    useEffect(() => {
-        setTodoAnim("end");
-    }, []);
+  useEffect(() => {
+    setTodoAnim("end");
+  }, []);
 
-    return (
-        <DragDropContext>
-            <Droppable droppableId={"droppable"}>
-                {(provided) => (
-                    <TodoList
-                        pose={todoAnim}
-                        className="todo-list"
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                    >
-                        {todos.map((todo, i) => (
-                            <Draggable draggableId={todo} index={i} key={todo}>
-                                {(provided) => (
-                                    <div
-                                        className="todo-item"
-                                        {...provided.dragHandleProps}
-                                        {...provided.draggableProps}
-                                        ref={provided.innerRef}
-                                    >
-                                        <TodoLogo className="todo-logo flex-center">{i + 1}</TodoLogo>
-                                        <div className="todo-text">
-                                            {todo}
-                                            <TodoVeil className="todo-veil" />
-                                        </div>
-                                    </div>
-                                )}
-                            </Draggable>
-                        ))}
-                        {provided.placeholder}
-                    </TodoList>
+  const dragEnd = result => {
+    console.log("Drag ended", result);
+  };
+
+  return (
+    <DragDropContext onDragEnd={dragEnd}>
+      <Droppable droppableId={"droppable"}>
+        {provided => (
+          <TodoList pose={todoAnim} className="todo-list" {...provided.droppableProps} ref={provided.innerRef}>
+            {todos.map((todo, i) => (
+              <Draggable draggableId={todo.text} index={i} key={todo.text}>
+                {provided => (
+                  <div
+                    className="todo-item"
+                    {...provided.dragHandleProps}
+                    {...provided.draggableProps}
+                    ref={provided.innerRef}
+                  >
+                    <TodoLogo className="todo-logo flex-center">{i + 1}</TodoLogo>
+                    <div className="todo-text">
+                      {/* <input type="text" value={todo} /> */}
+                      <TextareaAutosize className="todo-input" value={todo.text} />
+                      {/* <div>{todo}</div> */}
+                      <TodoVeil className="todo-veil" />
+                    </div>
+                  </div>
                 )}
-            </Droppable>
-        </DragDropContext>
-    );
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </TodoList>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 }
 
-const mapState = (state) => ({
-    todos: getTodos(state),
+const mapState = state => ({
+  todos: getTodos(state)
 });
 
 const mapDispatch = {
-    addTodo,
+  addTodo
 };
 
 export default connect(mapState, mapDispatch)(NoteList);
--- src/NoteList/state.js
+++ src/NoteList/state.js
@@ -5,32 +5,49 @@ const ADD_TODO = `${name}/ADD_TODO`;
 
 // Reducer
 const initialState = {
-    todos: ["display todos", "style them", "animate them", "implement 'add todo' functionality"],
+  todos: [
+    {
+      text: "display todos",
+      index: 1
+    },
+    {
+      text: "style them",
+      index: 2
+    },
+    {
+      text: "animate them",
+      index: 3
+    },
+    {
+      text: "implement 'add todo' functionality",
+      index: 4
+    }
+  ]
 };
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_TODO:
-            return {
-                ...state,
-                todos: [...state.todos, action.payload],
-            };
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [...state.todos, action.payload]
+      };
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
 // Action Creator
-export const addTodo = (payload) => ({
-    type: ADD_TODO,
-    payload,
+export const addTodo = payload => ({
+  type: ADD_TODO,
+  payload
 });
 
 // Selector
-export const getTodos = (state) => state[name].todos || initialState.todos;
+export const getTodos = state => state[name].todos || initialState.todos;
 
 export default {
-    reducer,
-    name,
+  reducer,
+  name
 };
